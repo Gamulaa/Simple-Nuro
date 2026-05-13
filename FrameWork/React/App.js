67271609e9270179c50a7c7083c9e183169fb36c
Gemechu Fekadu <fekadunathan372@gmail.com>
2026-05-13 20:04:39 +0300

    Create App.js

---
+++ FrameWork/React/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Simple Nuro</h1>
+      <p>Ethiopian Digital Marketplace</p>
+    </div>
+  );
+}
+
+export default App;
